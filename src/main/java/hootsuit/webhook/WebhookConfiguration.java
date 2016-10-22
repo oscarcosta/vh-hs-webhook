869a31e40244863128c5fb45fbf444b4045f8985
Oscar Costa <oscarcosta@gmail.com>
2016-10-22 00:32:23 -0200

    Initial Commit.

---
+++ src/main/java/hootsuit/webhook/WebhookConfiguration.java
@@ -0,0 +1,13 @@
+package hootsuit.webhook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebhookConfiguration {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(WebhookConfiguration.class, args);
+	}
+	
+}
