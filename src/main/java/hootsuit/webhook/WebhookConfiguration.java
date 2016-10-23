13cb8203e095e1411be3dbd49e15362c5911e973
Oscar Costa <oscarcosta@gmail.com>
2016-10-23 04:00:05 -0200

    Implemented Async method to send the messages to destinations

--- src/main/java/hootsuit/webhook/WebhookConfiguration.java
+++ src/main/java/hootsuit/webhook/WebhookConfiguration.java
@@ -1,19 +1,36 @@
 package hootsuit.webhook;
 
+import java.util.concurrent.Executor;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.AsyncConfigurerSupport;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableJpaRepositories("hootsuit.webhook.persistence")
 @EnableJpaAuditing
 @EnableTransactionManagement
-public class WebhookConfiguration {
+@EnableAsync
+public class WebhookConfiguration extends AsyncConfigurerSupport {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(WebhookConfiguration.class, args);
 	}
+	
+	@Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(2);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("MsgReqService-");
+        executor.initialize();
+        return executor;
+    }
 	
 }
